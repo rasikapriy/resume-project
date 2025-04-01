3f8c2d14d870cdb75377766bd01bfd9ad31ec501
rasikapriy <162892528+rasikapriy@users.noreply.github.com>
2025-04-01 14:20:13 +0530

    Update HoverCard.js

--- src/Components/HoverCard.js
+++ src/Components/HoverCard.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Card, Space, Tag, Typography } from "antd";
+import { Card, Space, Tag, Typography ,Button} from "antd";
 import "./Style.css";
 
 const {Title,Paragraph }=Typography;
-const HoverCard = ({ title, content, skills}) => {
+const HoverCard = ({ title, content, skills,buttonText,onClick}) => {
 
   const safeSkills = Array.isArray(skills) ? skills : [];
     return (
@@ -20,6 +20,7 @@ const HoverCard = ({ title, content, skills}) => {
           ))}
         </Space>
       )}
+{buttonText && onClick && (<Button type="primary" className="hover-card-button" onClick={onClick}>{buttonText}</Button>)}
     </Card>
   );
 };
